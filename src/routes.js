ff2a8f13bb5d570eddb34fa445419ac3c2081317
Warlem Lucio Lima <warlem@winfobh.com.br>
2019-10-30 12:52:20 -0300

    fix bug

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'World' });
+routes.get('/', async(req, res) => {
+    const user = await User.create({
+        name: 'Warlem Lima',
+        email: 'warlem@winfobh.com.br',
+        password_hash: '12345678',
+    });
+
+    return res.json(user);
 });
 
 export default routes;
