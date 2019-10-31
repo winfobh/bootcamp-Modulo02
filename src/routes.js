265af78bbad6af047ae68003661718c7cdf89909
Warlem L Lima <warlem@winfobh.com.br>
2019-10-30 23:19:34 -0300

    add controller User

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async(req, res) => {
-    const user = await User.create({
-        name: 'Warlem Lima',
-        email: 'warlem@winfobh.com.br',
-        password_hash: '12345678',
-    });
-
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
